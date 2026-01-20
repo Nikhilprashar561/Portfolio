b64a440fa448f6cf03e714e8dc756b1381960bba
nikhilprashar561 <nikhilprashar561@gmail.com>
2026-01-20 21:53:44 +0530

    Project Page Setup

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -2,84 +2,111 @@ import { Github, ExternalLink } from "lucide-react";
 
 const projects = [
   {
-    title: "Task Management System",
-    description:
-      "A React-based employee task management app to assign tasks and track progress.",
-    tech: ["React", "JavaScript", "Tailwind"],
-    github: "#",
-    live: "#",
-  },
-  {
-    title: "Authentication & Authorization",
-    description:
-      "Complete backend auth system using Node.js, Express, MongoDB with EJS frontend.",
-    tech: ["Node.js", "Express", "MongoDB", "EJS"],
-    github: "#",
-  },
-  {
-    title: "Password Manager",
-    description:
-      "Secure password manager built with React and styled using Tailwind CSS.",
-    tech: ["React", "Tailwind"],
-    github: "#",
-  },
-  {
-    title: "Blogging Website",
-    description:
-      "Authenticated blogging platform where logged-in users can create blogs.",
-    tech: ["Node.js", "MongoDB", "EJS"],
-    github: "#",
-  },
-  {
-    title: "NextJS Auth",
-    description: "Authentication & authorization system built with Next.js.",
-    tech: ["Next.js", "TypeScript"],
-    github: "#",
-  },
-  {
-    title: "Mystery Feedback",
-    description: "Next.js app for collecting anonymous user feedback.",
-    tech: ["Next.js", "TypeScript"],
-    github: "#",
-  },
-  {
     title: "SSM College Project",
     description:
       "MERN-based college management platform for students and teachers.",
     tech: ["MongoDB", "Express", "React", "Node.js"],
-    github: "#",
-  },
-  {
-    title: "Blinkit MERN Application",
-    description:
-      "Blinkit-like e-commerce app to practice real-world MERN workflows.",
-    tech: ["MERN Stack"],
-    github: "#",
-  },
-  {
-    title: "Learning DSA in JS",
-    description:
-      "JavaScript DSA repository to strengthen problem-solving skills.",
-    tech: ["JavaScript", "DSA"],
-    github: "#",
+    github: "https://github.com/Nikhilprashar561/SSM-College-Project",
   },
   {
     title: "MERN E-commerce",
     description:
       "Full-stack e-commerce app with authentication, cart, and orders.",
     tech: ["MERN Stack"],
-    github: "#",
+    github: "https://github.com/Nikhilprashar561/MERN-Ecommerce",
+  },
+  {
+    title: "Mystery Feedback",
+    description: "Next.js app for collecting anonymous user feedback.",
+    tech: ["Next.js", "TypeScript"],
+    github: "https://github.com/Nikhilprashar561/Mystry-Feedback",
+  },
+  {
+    title: "Blinkit MERN Application",
+    description:
+      "Blinkit-like e-commerce app to practice real-world MERN workflows.",
+    tech: ["MERN Stack"],
+    github: "https://github.com/Nikhilprashar561/Blinkit-MERN-Application",
+  },
+  {
+    title: "NextJS Auth",
+    description: "Authentication & authorization system built with Next.js.",
+    tech: ["Next.js", "TypeScript"],
+    github: "https://github.com/Nikhilprashar561/NextJS-Auth",
+  },
+  {
+    title: "Authentication & Authorization",
+    description:
+      "Complete backend auth system using Node.js, Express, MongoDB with EJS frontend.",
+    tech: ["Node.js", "Express", "MongoDB", "EJS"],
+    github: "https://github.com/Nikhilprashar561/Authentication-Authorization",
+  },
+  {
+    title: "Task Management System",
+    description:
+      "A React-based employee task management app to assign tasks and track progress.",
+    tech: ["React", "JavaScript", "Tailwind"],
+    github: "https://github.com/Nikhilprashar561/Task-Management",
+  },
+  {
+    title: "Password Manager",
+    description:
+      "Secure password manager built with React and styled using Tailwind CSS.",
+    tech: ["React", "Tailwind"],
+    github: "https://github.com/Nikhilprashar561/Password-Manager",
+    live: "https://reactpasword.netlify.app/",
+  },
+  {
+    title: "React Todo",
+    description:
+      "Secure password manager built with React and styled using Tailwind CSS.",
+    tech: ["React", "Tailwind"],
+    github: "https://github.com/Nikhilprashar561/React-Todo",
+    live: "https://reacttoddo.netlify.app/",
+  },
+  {
+    title: "Blogging Website",
+    description:
+      "Authenticated blogging platform where logged-in users can create blogs.",
+    tech: ["Node.js", "MongoDB", "EJS"],
+    github: "https://github.com/Nikhilprashar561/Blogging-Website",
+    live: "https://res.cloudinary.com/da9c3vejh/video/upload/Timeline_1_z4qubf.mp4?_s=vp-3.6.3",
+  },
+  {
+    title: "Amici Ashford",
+    description:
+      "Authenticated blogging platform where logged-in users can create blogs.",
+    tech: ["Node.js", "MongoDB", "EJS"],
+    github: "https://github.com/Nikhilprashar561/Amici-Ashford",
+    live: "https://amiciashford.netlify.app/",
+  },
+  {
+    title: "Online Shopping",
+    description:
+      "Authenticated blogging platform where logged-in users can create blogs.",
+    tech: ["Node.js", "MongoDB", "EJS"],
+    github: "https://github.com/Nikhilprashar561/Online-Shopping-Store",
+    live: "https://shopwithnikhil.netlify.app/",
+  },
+  {
+    title: "Travel Booking",
+    description:
+      "Authenticated blogging platform where logged-in users can create blogs.",
+    tech: ["Node.js", "MongoDB", "EJS"],
+    github: "https://github.com/Nikhilprashar561/Travel-Website",
+    live: "https://travelnikhil.netlify.app/",
   },
 ];
 
 export default function Projects() {
   return (
-    <section className="py-14 px-4 max-w-7xl mx-auto">
+    <section className="py-6 px-4 max-w-7xl mx-auto">
       {/* Section Header */}
       <div className="mb-12 text-center">
-        <p className="mt-3 text-gray-600 dark:text-gray-400 max-w-2xl mx-auto">
+        <p className=" text-gray-600 dark:text-gray-400 max-w-2xl mx-auto">
           A collection of full-stack, backend, and frontend projects showcasing
-          real-world development experience.
+          real-world development experience. A showcase of work created through
+          learning and real-world application.Browse the projects below.
         </p>
       </div>
 
